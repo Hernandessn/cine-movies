c48052eb77f7de7a81846a4920b059e30f0b08be
Hernandessn <hernandessalesneves7@gmail.com>
2025-02-11 10:01:56 -0300

    add Axios

--- src/Containers/Home/index.jsx
+++ src/Containers/Home/index.jsx
@@ -1,7 +1,15 @@
+import api from '../../services/api'
+
 
 export function Home(){
 
+    async function getMovies(){
+        const data = await api.get('/movies/popular')
 
+        console.log(data);
+        
+    }
+getMovies()
 
     return(
         <>
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import { GlobalStyles } from './styles/globalStyles';
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 import { Router } from './routes/routes'; 
 
 createRoot(document.getElementById('root')).render(
