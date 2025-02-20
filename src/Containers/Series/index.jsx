6673fa1720d0f20f8a4b85a7709e6ebe15f31d52
Hernandessn <hernandessalesneves7@gmail.com>
2025-02-19 22:50:52 -0300

    feat: estilizacao pagina de Séries

--- src/Containers/Series/index.jsx
+++ src/Containers/Series/index.jsx
@@ -1,13 +1,85 @@
+import { Button } from "../../components/Button";
+import { Modal } from "../../components/Modal";
+import { Slider } from "../../components/Slider";
+import {
+	getMovies,
+	getPopularSeries,
+	getTopMovies,
+	getTopPeople,
+	getTopSeries,
+} from "../../services/getData";
+import { getImages } from "../../utils/getImages";
+import { Background, Container, ContainerButton, Info, Poster } from "./styles";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export function Series() {
+	const [showModal, setShowModal] = useState(false);
+	const [movie, setMovie] = useState();
+	const [topSeries, setTopSeries] = useState();
+	const [popularSeries, setPopularSeries] = useState();
 
 
+	const navigate = useNavigate();
 
+	useEffect(() => {	
 
+		async function getAllData() {
+			Promise.all([
+				getMovies(),
+				getTopMovies(),
+				getTopSeries(),
+				getPopularSeries(),
+				getTopPeople(),
+			])
+				.then(([movie, topSeries, popularSeries]) => {
+					setMovie(movie);
+					setTopSeries(topSeries);
+					setPopularSeries(popularSeries);
+				})
+				.catch((error) => console.error(error));
+		}
 
-export function Series(){
+		getAllData();
+	}, []);
 
+	return (
+		<>
+			{movie && (
+				<Background img={getImages(movie.backdrop_path)}>
+					{showModal && (
+						<Modal movieId={movie.id} setShowModal={setShowModal} />
+					)}
+					<Container>
+						<Info>
+							<h1>{movie.title}</h1>
+							<p>{movie.overview}</p>
+							<ContainerButton>
+								<Button
+									onClick={() => navigate(`/detalhe/${movie.id}`)}
+									red={true}
+								>
+									Assista Agora
+								</Button>
+								<Button red={false} onClick={() => setShowModal(true)}>
+									Assista o Trailer
+								</Button>
+							</ContainerButton>
+						</Info>
 
+						<Poster>
+							<img alt="capa-do-filme" src={getImages(movie.poster_path)} />
+						</Poster>
+					</Container>
+				</Background>
+			)}
 
-    return(
-        <h1>Series</h1>
-    )
-};
+			
+			{topSeries && <Slider info={topSeries} title={"Top Séries"} />}
+			{popularSeries && (
+				<Slider info={popularSeries} title={"Séries Populares"} />
+			)}
+		
+		</>
+	);
+}
---
+++ src/Containers/Series/styles.js
@@ -0,0 +1,73 @@
+import styled, { keyframes } from "styled-components";
+
+const scale = keyframes`
+  from {
+    transform: scale(0);
+  }
+  to {
+    transform: scale(1);
+  }
+`;
+
+export const Background = styled.div`
+    background-image: url( ${ (props)=> props.img} );
+    height: 100vh;
+    background-position: center;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+
+    &::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0,0,0,0.5);
+        
+    }
+`;
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 100vh;
+    max-width: 1500px;
+`;
+
+export const Info = styled.div`
+    z-index: 2;
+    padding: 20px;
+    width: 50%;
+
+    h1{
+        font-size: 5rem;
+        font-weight: 700;
+        color: #ffff;
+    }
+    p{
+        font-size: 20px;
+        font-weight: 500;
+        color: #ffff;
+        margin-top: 30px;
+        margin-bottom: 20px;
+    }
+`;
+export const ContainerButton = styled.div`
+    display: flex;
+    gap: 20px;
+    margin-top: 30px;
+`;
+
+export const Poster = styled.div`
+    z-index: 4;
+       img{
+        width: 300px;
+        border-radius: 30px;
+        animation: ${scale} 0.5s linear;
+       }
+`;
