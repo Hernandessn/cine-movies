6a55e77f63d66c5b4525849bf44f1394da0c9cc5
Hernandessn <hernandessalesneves7@gmail.com>
2025-03-19 10:22:56 -0300

    feat: add responsiveness to button with font and padding adjustments for mobile

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,7 +1,6 @@
-import styled,{css} from "styled-components";
+import styled, { css } from "styled-components";
 
-
-const buttonStyles = css `
+const buttonStyles = css`
     border: 3px solid #ffff;
     background: transparent;
     color: #ffff;
@@ -10,31 +9,37 @@ const buttonStyles = css `
     cursor: pointer;
     font-size: 20px;
     font-weight: 500;
+    width: auto;
 
-    &:hover{
+    &:hover {
         color: #ff0000;
         background-color: #ffffff;
-
     }
-`
 
+    @media (max-width: 768px) {
+        font-size: 16px; /* Ajuste do tamanho da fonte em dispositivos móveis */
+        padding: 8px 16px; /* Ajuste do padding */
+    }
+
+    @media (max-width: 480px) {
+        font-size: 14px; /* Tamanho da fonte ainda menor para telas muito pequenas */
+        padding: 6px 12px; /* Padding reduzido para telas menores */
+    }
+`;
 
 export const ButtonWhite = styled.button`
     ${buttonStyles}
 `;
 
-
 export const ButtonRed = styled.button`
-     ${buttonStyles}
+    ${buttonStyles}
+    background: #ff0000;
+    border: 4px solid transparent;
+    box-shadow: 0px 0px 7px 8px rgb(255 0 0 / 30%);
 
-     background: #ff0000;
-     border: 4px solid transparent;
-     box-shadow: 0px 0px 7px 8px rgb(255 0 0 / 30%);
-
-
-     &:hover{
+    &:hover {
         box-shadow: 0px 0px 7px 15px rgb(255 0 0 / 30%);
         background: #ff0000;
         color: #ffff;
-     }
+    }
 `;
